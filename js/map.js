dd482a81c52e6e5aca25824e7639a76004d37fb5
NONstop5 <non_2002@mail.ru>
2018-11-15 20:04:08 +0300

    создает массив объектов объявлений

---
+++ js/map.js
@@ -0,0 +1,89 @@
+'use strict';
+
+var AVATAR_EXTENSION = '.png';
+var AVATAR_PATH = 'img/avatars/';
+
+// Функция генерации целого случайного числа из заданного диапазона
+var getRandomValueRange = function (minValue, maxValue) {
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+};
+
+// Функция перемешивает и возвращает, изначально, переданный массив
+var shuffleArray = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    var rndArrIndex = getRandomValueRange(0, array.length - 1);
+    var tmpValue = array[i];
+    array[i] = array[rndArrIndex];
+    array[rndArrIndex] = tmpValue;
+  }
+
+  return array;
+};
+
+var createAds = function () {
+  var titles = [
+    'Большая уютная квартира',
+    'Маленькая неуютная квартира',
+    'Огромный прекрасный дворец',
+    'Маленький ужасный дворец',
+    'Красивый гостевой домик',
+    'Некрасивый негостеприимный домик',
+    'Уютное бунгало далеко от моря',
+    'Неуютное бунгало по колено в воде'
+  ];
+  var types = [
+    'palace',
+    'flat',
+    'house',
+    'bungalo'
+  ];
+  var times = [
+    '12:00',
+    '13:00',
+    '14:00'
+  ];
+  var features = [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner'
+  ];
+  var photos = [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+  ];
+  var result = [];
+
+  for (var i = 0; i < 8; i++) {
+    var ad = {
+      author: {
+        avatar: AVATAR_PATH + 'user0' + (i + 1) + AVATAR_EXTENSION // строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
+      },
+      offer: {
+        title: titles[i], // строка, заголовок предложения, одно из фиксированных значений. Значения не должны повторяться.
+        address: getRandomValueRange(0, 1000) + ', ' + getRandomValueRange(0, 1000), // строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
+        price: getRandomValueRange(1000, 1000000), // число, случайная цена от 1000 до 1000000
+        type: types[getRandomValueRange(0, 3)], // строка с одним из четырёх фиксированных значений:
+        rooms: getRandomValueRange(1, 5), // число, случайное количество комнат от 1 до 5
+        guests: getRandomValueRange(1, 10), // число, случайное количество гостей, которое можно разместить
+        checkin: times[getRandomValueRange(0, 2)], // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+        checkout: times[getRandomValueRange(0, 2)], // строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+        features: features.slice(getRandomValueRange(1, features.length - 1)), // массив строк случайной длины из ниже предложенных:
+        description: '', // пустая строка
+        photos: shuffleArray(photos) // массив из строк ... расположенных в произвольном порядке
+      },
+      location: {
+        x: getRandomValueRange(1, 10), // случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
+        y: getRandomValueRange(130, 630) // случайное число, координата y метки на карте от 130 до 630.
+      }
+    };
+    result.push(ad);
+  }
+
+  return result;
+};
+
+var ads = createAds();
